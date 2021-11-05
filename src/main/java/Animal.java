d1c4008e38c2271808018dea3f4a7d03ccf4ca17
echwaadams <adamsechwa18@gmail.com>
2021-11-05 11:52:22 +0300

    created get method to obtain animal name and its test

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,17 @@
 public class Animal {
+    private String name;
+    private String type;
+    public static final String ANIMALS_TYPE = "animal";
+    public Animal(String name) {
+        this.name = name;
+        this.type = ANIMALS_TYPE;
+    }
 
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,22 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Test
+    public void instantiatesCorrectly_true(){
+        Animal testAnimal = new Animal("wolf");
+        assertEquals(true, testAnimal instanceof Animal);
+    }
+    @Test
+    public void Animals_instantiatesCorrectlyWithType_String(){
+        Animal testAnimal = new Animal("wolf");
+        assertEquals("animal",testAnimal.getType());
+    }
+    @Test
+    public void animal_instantiatesWithName_String(){
+        Animal testAnimal = new Animal("wolf");
+        assertEquals("wolf",testAnimal.getName());
+    }
 
 }

ce4423ebfa28f0c1392eece686989f63516aaca6
echwaadams <adamsechwa18@gmail.com>
2021-11-06 22:23:32 +0300

    will add all template to repo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,12 @@
+import static spark.Spark.*;
+
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
 public class App {
+    public static void main(String[] args) {
+
+    }
 }

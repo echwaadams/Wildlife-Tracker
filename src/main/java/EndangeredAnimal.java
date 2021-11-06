48731b3edff3ee0de82be3a2d352b81cfd4c6781
echwaadams <adamsechwa18@gmail.com>
2021-11-06 18:24:00 +0300

    implemented constants

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,4 +1,18 @@
-public class EndangeredAnimal extends Animal {
+public class EndangeredAnimal extends Animal implements {
+    private String health;
+    private String age;
+
+    public static final String ANIMAL_TYPE = "endangered";
+    public static final String ADULT = "adult";
+    public static final String YOUNG = "youthful";
+    public static final String NEWBORN = "newborn";
+    public static final String HEALTHY = "very healthy";
+    public static final String AVERAGE = "averagely unhealthy";
+    public static final String ILL = "very unhealthy";
     public EndangeredAnimal(String name) {
+        super(name);
+        this.type = ANIMAL_TYPE;
+        this.health = health;
+        this.age = age;
     }
 }

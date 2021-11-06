4127e006bf0f0319c2cbd288f03a6134ab161e0a
echwaadams <adamsechwa18@gmail.com>
2021-11-06 13:56:50 +0300

    created database

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,9 @@
+import java.util.Objects;
+import org.sql2o.*;
+
 public class Animal {
     private String name;
+    private int id;
     private String type;
     public static final String ANIMALS_TYPE = "animal";
     public Animal(String name) {
@@ -14,4 +18,33 @@ public class Animal {
     public String getName() {
         return name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    // Overriding Methods
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+
+        Animal animal = (Animal) o;
+        if(getId() != animal.getId()) return false;
+        if (!getName().equals(animal.getName())) return false;
+        return getType().equals(animal.getType());
+    }
+
+    // database setup
+//    @Override
+//    public void save(){
+//        try(Connection con = DB.sql2o.open()){
+//            String sql = "INSERT INTO animals(name,type) VALUES(:name,:type)";
+//                    this.id = (int) con.createQuery(sql, true)
+//                            .addParameter("name",this.name)
+//                            .addParameter("type",this.type)
+//                            .executeUpdate()
+//                            .getKey();
+//        }
+//    }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,8 +1,15 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+//
+//    @Rule
+//    public DatabaseRule database = new DatabaseRule();
+//
     @Test
     public void instantiatesCorrectly_true(){
         Animal testAnimal = new Animal("wolf");
@@ -17,6 +24,22 @@ public class AnimalTest {
     public void animal_instantiatesWithName_String(){
         Animal testAnimal = new Animal("wolf");
         assertEquals("wolf",testAnimal.getName());
+    }
+
+    // overriding equals
+    @Test
+    public void equals_returnsTrueIfAnimalsAreSame(){
+        Animal firstAnimal = new Animal("wolf");
+        Animal secondAnimal = new Animal("wolf");
+        assertTrue(firstAnimal.equals(secondAnimal));
+    }
+
+    //database setup
+    @Test
+    public void save_CorrectlyIntoTheDatabase(){
+        Animal testAnimal = new Animal("wolf");
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
     }
 
 }

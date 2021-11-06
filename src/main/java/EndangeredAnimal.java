b4207c4c26ce995b6ed037423d3e87b605de289d
echwaadams <adamsechwa18@gmail.com>
2021-11-06 18:18:28 +0300

    created constructor for endangered animals

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,3 +1,4 @@
 public class EndangeredAnimal {
-
+    public EndangeredAnimal() {
+    }
 }
--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,5 +1,16 @@
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredAnimalTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void enAnimal_InstantiatesCorrectly_true(){
+        EndangeredAnimal endangeredAnimal = setUpTheTask();
+        assertEquals(true,endangeredAnimal instanceof EndangeredAnimal);
+    }
 
 }
